6dfba9016e2281ad6526817fa6f77c5ee7378207
ngocphule6799 <ngocphule6799@gmail.com>
2022-06-26 10:03:44 +0700

    base project

---
+++ Day6-Exercise-Shooting-Fish-Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day6-Exercise-Shooting-Fish-Game
+//
+//  Created by Le Ngoc Phu on 26/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
